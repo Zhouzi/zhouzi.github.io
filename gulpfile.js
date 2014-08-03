223e65a2c9a5797a11baa6d71687980abc07e444
Gabin <hello@gabinaureche.com>
2014-08-03 16:57:51 +0400

    support for firefox > 20

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var gulp          = require( 'gulp' ),
 gulp.task( 'styles', function () {
   return gulp.src( scssPath )
     .pipe( sass( { style : 'compressed' } ) )
-    .pipe( autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ) )
+    .pipe( autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4', 'Firefox > 20' ) )
     .pipe( gulp.dest( basePath + 'css' ) );
 });
 
