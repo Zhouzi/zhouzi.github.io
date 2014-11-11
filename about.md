3f49f83118487761c994b2fd9e39b33751c725e5
Gabin <hello@gabinaureche.com>
2014-11-11 01:56:35 +0100

    brand new version

--- about.md
+++ about.md
@@ -1,11 +1,17 @@
 ---
-layout: page
-title: About
+layout: about
+title: About Gabin Aureche
 permalink: /about/
 ---
 
-This is the base Jekyll theme. You can find out more info about customizing your Jekyll theme, as well as basic Jekyll usage documentation at [jekyllrb.com](http://jekyllrb.com/)
+It was a time when I wasn't sure whether I was a **designer** or a **developer**.
+Back then it seemed hard to conciliate both, I had to choose between **thinking** or **executing***.
 
-You can find the source code for the Jekyll new theme at: [github.com/jglovier/jekyll-new](https://github.com/jglovier/jekyll-new)
+But you know what? I didn't, I just continued to do what I love: **building experiences**.
+And there is a name for that kind of job nowadays: **front-end developer**.
 
-You can find the source code for Jekyll at [github.com/jekyll/jekyll](https://github.com/jekyll/jekyll)
+To me, a front-end developer's role is not just to **make things happen** but to ensure that
+**user experience is delightful**, no matter on what device. To do so I do my best to **keep technical
+considerations out of the way** to build **user-first** products [@wizbii](http://wizbii.com).
+
+_*No offense, just making things kind of dramatic._
--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,40 @@
-var basePath    = '',
-    scssPath    = basePath + 'css/scss/**/*.scss',
-    jsBasePath  = basePath + 'js/partials/',
-    jsPath      = jsBasePath + '**/*.js';
+var gulp = require("gulp"),
+    sass = require("gulp-ruby-sass"),
+    autoprefixer = require("gulp-autoprefixer");
 
-// Load plugins
-var gulp          = require( 'gulp' ),
-    sass          = require( 'gulp-ruby-sass' ),
-    autoprefixer  = require( 'gulp-autoprefixer' ),
-    concat        = require( 'gulp-concat' ),
-    livereload    = require( 'gulp-livereload' );
-
-// Styles
-gulp.task( 'styles', function () {
-  return gulp.src( scssPath )
-    .pipe( sass( { style : 'compressed' } ) )
-    .pipe( autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4', 'Firefox > 20' ) )
-    .pipe( gulp.dest( basePath + 'css' ) );
+gulp.task("styles", function () {
+    return gulp
+        .src("_sass/**/*.scss")
+        .pipe(sass({
+            style: "nested"
+        }))
+        .pipe(gulp.dest("css"));
 });
 
-// Scripts
-gulp.task( 'scripts', function () {
-  return gulp.src( [ 
-      jsBasePath + 'type.js',
-      jsBasePath + 'chattybar.js',
-      jsBasePath + 'main.js'
-    ] )
-    .pipe( concat( 'scripts.js' ) )
-    .pipe( gulp.dest( basePath + 'js' ) );
-});
 
-// Default task
-gulp.task( 'default', function() {
-    gulp.start( 'styles', 'scripts' );
+gulp.task( 'watch', function () {
+    gulp.watch("_sass/**/*.scss", ["styles"]);
 } );
 
-// Watch
-gulp.task( 'watch', function () {
-  // Watch .scss files
-  gulp.watch( scssPath, [ 'styles' ] );
-  
-  // Watch .js files
-  gulp.watch( jsPath, [ 'scripts' ] );
-  
-  // Watch any files and reload on change
-  var server = livereload();
-  gulp.watch( 'app/**' ).on( 'change', function ( file ) {
-    server.changed( file.path );
-  } );
+
+gulp.task("default", function () {
+    gulp.run(["styles"]);
+});
+
+
+gulp.task("build", function () {
+    gulp
+        .src("_sass/**/*.scss")
+        .pipe(sass({
+            style: "compressed"
+        }))
+        .pipe(autoprefixer(
+            "last 2 version",
+            "safari > 4",
+            "ie > 7",
+            "opera > 12",
+            "ios > 5",
+            "android > 3",
+            "Firefox > 20" ))
+        .pipe(gulp.dest("css"));
 });
